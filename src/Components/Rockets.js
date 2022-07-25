aac974d4c9d0303e457700fe2a5c08492995f326
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-25 18:15:07 +0300

    Fix dispatch those actions once when changing views using navigation.

--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
+
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux/es/exports';
-import { loadRocketsThunk, rocketsReducer } from "../Redux/Rockets/Rockets";
+import { rocketsReducer } from "../Redux/Rockets/Rockets";
 
 const Rockets = () => {
   // get rockets data from the store
   const rockets = useSelector(state => state.rockets);
+
   const dispatch = useDispatch();
+
   const handle = e => {
     const id = e.target.id;
     dispatch(rocketsReducer.ReservationAction({ id }))
   }
-
-  useEffect(() => {
-    dispatch(loadRocketsThunk());
-  }, []);
 
   return (
     <section className="rockets">
@@ -22,14 +21,16 @@ const Rockets = () => {
           <div className="rocket-card" key={rocket.rocket_id}>
             <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
             {rocket.reserved && (<span>ROCKET REVERVED!!!!!!</span>)}
-            <h2>{rocket.rocket_name}</h2>
-            <p>{rocket.description}</p>
-            {!rocket.reserved && (<button type='button'
-              id={rocket.rocket_id}
-              onClick={handle}>Reserve</button>)}
-            {rocket.reserved && (<button type='button'
-              id={rocket.rocket_id}
-              onClick={handle}>Cancel Reservation</button>)}
+            <div>
+              <h2>{rocket.rocket_name}</h2>
+              <p>{rocket.description}</p>
+              {!rocket.reserved && (<button type='button'
+                id={rocket.rocket_id}
+                onClick={handle}>Reserve</button>)}
+              {rocket.reserved && (<button type='button'
+                id={rocket.rocket_id}
+                onClick={handle}>Cancel Reservation</button>)}
+            </div>
           </div>
         )
       })}
