1b280606904e1b2733a56d465cba422039e3053f
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-07-25 12:28:31 +0300

    Display the rockets data In the UI.

--- src/Components/Rockets.js
+++ src/Components/Rockets.js
@@ -1,10 +1,28 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { loadRocketsThunk } from "../Redux/Rockets/Rockets";
 
 const Rockets = () => {
+  // get rockets data from the store
+  const rockets = useSelector(state => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRocketsThunk());
+  }, []);
+
   return (
     <section className="rockets">
-
+      {rockets.map((rocket) => {
+        return (
+          <div className="rocket-card" key={rocket.rocket_id}>
+            <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+            <h2>{rocket.rocket_name}</h2>
+            <p>{rocket.description}</p>
+            <button type='button' handel></button>
+          </div>
+        )
+      })}
     </section>
 
   )
